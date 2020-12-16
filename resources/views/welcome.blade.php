21267c6b17b8c68b41db8fc9d9251c7bbc465093
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2020-12-15 20:49:46 -0500

    update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content= "width=device-width, user-scalable=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Curriculumn</title>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+</head>
+<body class="c-app">
+    <div id="app" class="w-100">
+        <div class="c-wrapper">
+            <div class="c-body">
+                <main class="c-main">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-success fixed-top" id="sideNav">
+                        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                          <span class="d-none d-lg-block">
+                            <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="/images/profile.jpg" alt="">
+                          </span>
+                        </a>
+                        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                          <ul class="navbar-nav">
+                            <li class="nav-item">
+                              <a class="nav-link js-scroll-trigger active" href="#about">Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link js-scroll-trigger" href="#experience">Experiencia</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link js-scroll-trigger" href="#education">Educaci&oacute;n</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link js-scroll-trigger" href="#skills">Habilidades</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link js-scroll-trigger" href="#interests">Intereses</a>
+                            </li>
+                          </ul>
+                        </div>
+                      </nav>
+                  
+                      <div class="container-fluid p-0">
+                  
+                        <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+                          <div class="my-auto">
+                            <h1 class="mb-0">Nicolas
+                              <span class="text-success">Garcia Ramirez</span>
+                            </h1>
+                            <div class="subheading mb-5">Campeste C · Dosquebradas · 322-6079759 ·
+                              <a href="mailto:name@email.com" class="text-success">desarrollo.dev.ni@gmail.com</a>
+                            </div>
+                            <p class="mb-5">Tengo experiencia en el desarrollo de aplicaciones web, manejo de frameworks como laravel, vue.js, uno de los proyectos en los que he trabajado es <a href="https://www.noisesharks.com/">Noisesharks</a>, tambien soy muy bueno trabajando en equipo, acatando ordenes para cumplir todos los objetivos planteados, manejo la metodologia scrum. </p>
+                            <ul class="list-inline list-social-icons mb-0">
+                              <img src="a_data/social.png">
+                            </ul>
+                          </div>
+                        </section>
+                  
+                        <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+                          <div class="my-auto">
+                            <h2 class="mb-5">Experiencia</h2>
+                  
+                            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                              <div class="resume-content mr-auto">
+                                <h3 class="mb-0">Tecnico en Mantenimiento de equipos de computo</h3><br>
+                                <div class="subheading mb-3">Apostar S.A</div>
+                                <p>mantenimiento preventivo y proactivo de equipos de computo, soporte
+                                de computacion, mantenimiento a impresoras.<br> Instalacion de puntos de venta
+                                configuracion de equipos, e instalacion de cableado estructurado.</p>
+                              </div>
+                              <div class="resume-date text-md-right">
+                                <span class="text-success">15 06 2017 - 31 12 2017</span>
+                              </div>
+                            </div>
+                            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                              <div class="resume-content mr-auto">
+                                <h3 class="mb-0">Developer Jr</h3><br>
+                                <div class="subheading mb-3">Accedo Technologies</div>
+                                <p>desarrollo de aplicaciones web, con el manejo de frameworks (laravel, vue.js), dessarollo backend y diseno de frontend</p>
+                              </div>
+                              <div class="resume-date text-md-right">
+                                <span class="text-success">01 04 2018 - Actualmente</span>
+                              </div>
+                            </div>
+                  
+                        </section>
+                  
+                        <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+                          <div class="my-auto">
+                            <h2 class="mb-5">Educaci&oacute;n</h2>
+                  
+                            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                              <div class="resume-content mr-auto">
+                                <h3 class="mb-0">Tecnico En Mantenimiento De Equipos De Computo</h3><br>
+                                <div class="subheading mb-3">SENA</div>
+                                <div>CENTRO DE DISENO E INNOVACION TECNOLOGICA INDUSTRIAL</div>
+                              </div>
+                              <div class="resume-date text-md-right">
+                                <span class="text-success">Abril 2018 - Diciembre 2010</span>
+                              </div>
+                            </div>
+                  
+                            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                              <div class="resume-content mr-auto">
+                                <h3 class="mb-0">Tecnologo en Analisis y Desarrollo De Sistemas de Informacion</h3><br>
+                                <div class="subheading mb-3">SENA</div>
+                                <div>CENTRO DE DISENO E INNOVACION TECNOLOGICA INDUSTRIAL</div>
+                              </div>
+                              <div class="resume-date text-md-right">
+                                <span class="text-success">Abril 2018 - abril 2020</span>
+                              </div>
+                            </div>
+                  
+                          </div>
+                        </section>
+                  
+                        <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+                          <div class="my-auto">
+                            <h2 class="mb-5">Habilidades</h2>
+                  
+                            <div class="subheading mb-3">Lenguajes de Programacion &amp; Herramientas</div>
+                  
+                            <div class="subheading mb-3"></div>
+                            <ul class="fa-ul mb-0">
+                              <li>&#9658; Diseño responsibe</li>
+                              <li>&#9658; Diseño frontend &amp; CSS HTML5 JavaScript</li>
+                              <li>&#9658; Desarrollo backend &amp; Python PHP </li>
+                              <li>&#9658; Frameworks &amp; Laravel, Django, Vue.js</li>
+                              <li>&#9658; Instalacion de Servidores &amp; servicios web, servicios de correo, servicios DNS, servicios PXE...</li>
+                              <li>&#9658; Desarrollo Agil &amp; Scrum</li>
+                            </ul>
+                          </div>
+                        </section>
+                  
+                        <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+                          <div class="my-auto">
+                            <h2 class="mb-5">Intereses</h2>
+                            <p>Me intereso por mejorar cada dia, alcanzar mis metas y ser el mejor en lo que hago
+                            busco y me actualizo de las nuevas herramientas y metodologias de trabajo.<br>Soy proactivo y 
+                            siempre doy mi opinion para una mejor solucion.</p>
+                          </div>
+                        </section>
+                      </div>
+                </main>
+            </div>
+        </div>
+    </div>
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="{{ mix('/js/app.js') }}"></script>
+    <script src="{{ asset('/js/pace.min.js') }}" charset="utf-8"></script>
+</body>
+</html>
