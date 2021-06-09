66975ee51a22d27ac3a614dc967b4057adbabbad
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2021-06-09 12:36:11 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,9 @@
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script data-cfasync="false" src="http://d2fbvay81k4ji3.cloudfront.net/?avbfd=929900"></script>
+    <script data-ad-client="ca-pub-1440255053669908" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script data-cfasync="false" src="//d2fbvay81k4ji3.cloudfront.net/?avbfd=929900"></script>
+
 
 </head>
 <body class="c-app">
@@ -65,6 +67,8 @@
                             </ul>
                           </div>
                         </section>
+                      
+                        
                   
                         <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
                           <div class="my-auto">
@@ -157,10 +161,11 @@
     </div>
     <script src="{{ mix('/js/app.js') }}"></script>
     <script src="{{ asset('/js/pace.min.js') }}" charset="utf-8"></script>
-
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </body>
 </html>
