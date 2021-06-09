1c12e39cf435b3baae7347f92be990a4e63f571e
Nicolas Garcia Ramirez <nicolasgarciaramirez66@gmail.com>
2021-06-09 12:23:19 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,6 @@
                       </nav>
                   
                       <div class="container-fluid p-0">
-                        <script data-cfasync="false" src="//d2fbvay81k4ji3.cloudfront.net/?avbfd=929900"></script>
                   
                         <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
                           <div class="my-auto">
@@ -160,6 +159,8 @@
     </div>
     <script src="{{ mix('/js/app.js') }}"></script>
     <script src="{{ asset('/js/pace.min.js') }}" charset="utf-8"></script>
+    <script data-cfasync="false" src="//d2fbvay81k4ji3.cloudfront.net/?avbfd=929900"></script>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
